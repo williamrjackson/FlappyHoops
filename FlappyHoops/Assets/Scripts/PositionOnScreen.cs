e171f3003f327ac7a863b8cd766cc33ff246d91f
Bill Jackson <williamrjackson@gmail.com>
2017-12-04 13:41:04 -0500

    Allow position offscreen. Optional continuous positioning.

--- FlappyHoops/Assets/Scripts/PositionOnScreen.cs
+++ FlappyHoops/Assets/Scripts/PositionOnScreen.cs
@@ -3,15 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class PositionOnScreen : MonoBehaviour {
-    [Range(0, 1)]
+    [Range(-1, 2)]
     public float xOffset = .5f;
-    [Range(0, 1)]
+    [Range(-1, 2)]
     public float yOffset = .5f;
+    public bool onUpdate;
 
+    private void Start()
+    {
+        SetPosition();
+    }
     // Use this for initialization
     void Update () {
-        float screenX = Camera.main.ScreenToWorldPoint(new Vector3(Mathf.Lerp(0, Screen.width, xOffset), transform.position.y, transform.position.z)).x;
-        float screenY = Camera.main.ScreenToWorldPoint(new Vector3(transform.position.x, Mathf.Lerp(0, Screen.height, yOffset), transform.position.z)).y;
+        if (onUpdate)
+        {
+            SetPosition();
+        }
+    }
+    void SetPosition()
+    {
+        float screenX = Camera.main.ScreenToWorldPoint(new Vector3(Mathf.LerpUnclamped(0, Screen.width, xOffset), transform.position.y, transform.position.z)).x;
+        float screenY = Camera.main.ScreenToWorldPoint(new Vector3(transform.position.x, Mathf.LerpUnclamped(0, Screen.height, yOffset), transform.position.z)).y;
         transform.position = new Vector3(screenX, screenY, transform.position.z);
     }
 }
